d88b11ff70547945e79ba92e086b5e32d7777a26
albertof89 <34237445+albertof89@users.noreply.github.com>
2017-12-07 14:14:21 +0000

    Add files via upload

---
+++ CMakeFiles/test_cv_camera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_cv_camera.dir/MultipleCameraEx.cpp.o"
+  "test_cv_camera.pdb"
+  "test_cv_camera"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_cv_camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
